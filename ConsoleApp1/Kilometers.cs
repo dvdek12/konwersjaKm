d5ed22e8eba4e08a93964d0fba7cebbe5d0773fc
buzz <buzz@XCB-PC-TRAINEE2>
2022-07-18 11:09:13 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Kilometers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Kilometers
+    {
+        public int value;
+        public Kilometers(int value)
+        {
+            this.value = value;
+        }
+           
+        public void SaveValueToFile(string path, int value)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write(value.ToString());
+            sw.Close();
+        }
+
+        public int ConvertToMeters()
+        {
+            return this.value * 1000;
+        }
+
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Podaj dystans w km: ");
+
+Kilometers distance = new Kilometers(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine($"{distance.value} km => {distance.ConvertToMeters()} m");
+
+distance.SaveValueToFile(@"C:\Users\buzz\source\repos\ConsoleApp1\ConsoleApp1\file.txt", distance.ConvertToMeters());
+
+
+
